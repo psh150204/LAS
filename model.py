a19208acf3499a6bd6afd3f096721eaac9c9cc23
psh150204 <psh131226@gmail.com>
2020-07-10 12:44:35 +0900

    model.py - add Listener and Speller

--- model.py
+++ model.py
@@ -1,4 +1,20 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 import numpy as np
+
+class Listener(nn.Module):
+    def __init__(self):
+        super(Listener, self).__init__()
+
+    def forward(self, x):
+        pass
+
+
+class Speller(nn.Module):
+    def __init__(self):
+        super(Speller, self).__init__()
+
+    def forward(self, x):
+        pass
