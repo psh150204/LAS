571b9b9deef6d90811e2e3630b7a70b4a59a8309
psh150204 <psh131226@gmail.com>
2020-07-13 14:32:45 +0900

    model.py - Update Speller

--- model.py
+++ model.py
@@ -20,22 +20,40 @@ class Listener(nn.Module):
         bs = x.size(0)
 
         # bottom bidirectional LSTM
-        h, _ = self.BLSTM(x) # batch * seq * h_dim
+        h, _ = self.BLSTM(x) # [batch, seq, h_dim]
 
         # pyramid BLSTM for reducing time resolution
         for i in range(N):
             seq = h.size(1)
-            h = h.view(bs, seq // 2, h_dim * 2) # batch * seq/2 * (h_dim * 2)
+            h = h.view(bs, seq // 2, h_dim * 2) # [batch, seq/(2**(i+1)), (h_dim * 2)]
             h, _ = self.pBLSTM_layers(h)
         
-        return h
+        return h # [batch, seq/(2**N), h_dim]
 
+class OneHotVectorEncoding(nn.Module):
+    def __init__(self, vocab_size):
+        super(OneHotVectorEncoding, self).__init__()
+        self.dim = vocab_size
+
+    def forward(self, x):
+        # input : a tensor with size [batch, seq]
+        batch_size = x.size(0)
+        sentence_length = x.size(1)
+
+        # embedding
+        one_hot_encoding = Variable(torch.zeros(batch_size, sentence_length, self.dim))
+        #one_hot_encoding = Variable(torch.zeros(batch_size, sentence_length, self.dim)).cuda()
+        for i in range(batch_size):
+            for j in range(sentence_length):
+                one_hot_encoding[i][j][x[i][j]] = 1
+
+        return one_hot_encoding # [batch, seq, vocab_size]
 
 class AttentionContext(nn.Module):
-    def __init__(self):
+    def __init__(self, s_dim, h_dim, dim):
         super(AttentionContext, self).__init__()
-        self.phi = nn.Linear()
-        self.psi = nn.Linear()
+        self.phi = nn.Linear(s_dim, dim)
+        self.psi = nn.Linear(h_dim, dim)
 
     def forward(self, s, h):
         # input
@@ -49,16 +67,73 @@ class AttentionContext(nn.Module):
         alpha = F.softmax(e, dim = -1)
         c = torch.einsum('ij,ijk->ij', alpha, h) # [batch, seq, h_dim]
 
-        return c
+        return c # [batch, seq, h_dim]
         
 
 class Speller(nn.Module):
-    def __init__(self):
+    def __init__(self, s_dim, h_dim, dim, embedding, sampling_rate = 0.1):
         super(Speller, self).__init__()
-        self.AttentionContext = AttentionContext()
-        self.RNN = nn.LSTM(num_layers = 2)
+        self.AttentionContext = AttentionContext(s_dim, h_dim, dim)
+        self.first_RNN = nn.LSTMCell()
+        self.second_RNN = nn.LSTM()
         self.CharacterDistribution = nn.Linear()
-    def forward(self, x):
-        pass
+        self.sampling_rate = sampling_rate
+        self.embedding = embedding
+
+    def decode(self, probs):
+        
+
+    def forward(self, Y, h, ground_truth = None):
+        # input
+        # Y = batch * [<sos>, y1, ..., y_(s-1)]
+        # ground_truth = batch * [<sos>, y1, ..., y_n, <eos>]
+        # output : Y' = batch * [y1, ..., y_s] / y_s can be <eos>
+        
+        s = torch.zeros() # hidden state
+        cs = torch.zeros() # cell state
+        probs = []
+
+        for i in range(len(y)):
+            isTruth = False
+
+            # training
+            if ground_truth != None and np.random.uniform(0,1,1)[0] > 0.1 :
+                isTruth = True
+
+            if isTruth:
+                y = self.embedding(ground_truth[:, i])
+            else:
+                y = self.embedding(Y[:, i]) # [batch, vocab_size]
+            
+            s, cs = self.first_RNN(y, (s,cs))
+            s = self.AttentionContext(s, h)
+            s, _ = self.second_RNN(s)
+            p = F.softmax(self.CharacterDistribution(s), dim = -1)
+            probs.append(p)
+        
+        return decode(probs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
